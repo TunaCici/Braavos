a11e70ad534633d3dd0e610509c12be632204bc1
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-08-02 22:03:25 +0300

    interpreter: ls command "-la" arguments.
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

--- src/modules/Terminal/Interpreter.js
+++ src/modules/Terminal/Interpreter.js
@@ -12,13 +12,40 @@ function echo(args) {
 function ls(args) {
   let retMsg = "";
 
-  if (args.length === 0) {
-    for (let node in virtfs) {
-      retMsg += node + " ";
+  let seperator = " ";
+  let listDir = "."
+  let listAll = false;
+
+  /* Parse arguments */
+  if (args.length > 0) {
+    if (!args[0].startsWith("-")) {
+      listDir = args[0];
+    }
+
+    for (let i = 0; i < args.length; i++) {
+      if (args[i].startsWith("-")) {
+        if (args[i].includes("a")) {
+          listAll = true;
+        }
+
+        if (args[i].includes("l")) {
+          seperator = "\n";
+        }
+      }
+    }
+  }
+
+  if (listDir === ".") {
+    for (let node in virtfs) {  
+      if (listAll || !node.startsWith(".")) {
+        retMsg += node + seperator;
+      }
     }
   } else {
-    for (let node in virtfs[args[0]]) {
-      retMsg += node + " ";
+    for (let node in virtfs[listDir]) {
+      if (listAll || !node.startsWith(".")) {
+        retMsg += node + seperator;
+      }
     }
   }
 
