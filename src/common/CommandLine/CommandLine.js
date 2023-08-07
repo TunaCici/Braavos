209c699b44f29719a219b5f5411734d33396425b
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-08-07 23:12:33 +0300

    commandline: Wrap it around a div.
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

--- src/common/CommandLine/CommandLine.js
+++ src/common/CommandLine/CommandLine.js
@@ -3,7 +3,11 @@ import React, { useState, useEffect } from "react";
 import "./CommandLine.css";
 
 function CommandLine(props) {
-  return <p className="command-line">{props.text}</p>;
+  return (
+    <div className="command-line">
+      {props.text}
+    </div>
+  )
 }
 
 export default CommandLine;
