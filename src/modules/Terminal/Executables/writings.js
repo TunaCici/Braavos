b749d28d9c625ab11d2846e008da30e19a581920
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-08-17 21:44:50 +0300

    wrritings: Relatively better formatting.
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

--- src/modules/Terminal/Executables/writings.js
+++ src/modules/Terminal/Executables/writings.js
@@ -10,16 +10,19 @@ function writings(args) {
   let availableWritings = [];
 
   for (let i = 0; i < writingsJSON["writings"].length; i++) {
+    const leadingSpaces = 42;
+
     let writing = writingsJSON["writings"][i];
 
     let name = writing["name"];
     let date = writing["date"];
+    let tags = writing["tags"];
     let url = writing["url"];
 
-    let leadingSpaces = 48 - name.length;
-    date = " ".repeat(leadingSpaces) + "- " + date;
+    let fillCount = leadingSpaces - name.length;
+    date = ".".repeat(fillCount) + "↦ " + date;
 
-    let writingHTML = (
+    let writingHeader = (
       <div key={"writings-" + i} className="writings">
         <div key={"writings-no-" + i}>{i} </div>
         <a href={url} className="cool-blue" target="_blank">{name}</a>
@@ -27,8 +30,36 @@ function writings(args) {
       </div>
     );
 
-    availableWritings.push(writingHTML);
+    let eyeCandyKeywords = " ".repeat(leadingSpaces + 2) + "↳ ";
+    let tagSpans = [];
+
+    tagSpans.push(<span key={"writings-tags-space-x"}>{eyeCandyKeywords}</span>);
+
+    for (let j = 0; j < tags.length; j++) {
+      let tag = tags[j];
+
+      tagSpans.push(<span key={"writings-tags-" + i + "-" + j} className="cool-green thin-text">#{tag} </span>);
+    }
+
+    let writingsDetails = (
+      <div key={"writings-details-" + i} className="writings-details">
+        {tagSpans}
+      </div>
+    );
+
+    availableWritings.push(writingHeader);
+    availableWritings.push(writingsDetails);
   }
+
+  const writingsFooter = (
+    <p key={"writings-footer"} className="writings-p">More available on my
+      <span> </span>
+      <a className="cool-green" href="https://tunacici.com/blog/">Blog</a> collection-thingy 🩸
+    </p>
+  );
+
+  availableWritings.push(<br key={"writings-br"} />);
+  availableWritings.push(writingsFooter);
 
   return (
     <div id="writings-output">
